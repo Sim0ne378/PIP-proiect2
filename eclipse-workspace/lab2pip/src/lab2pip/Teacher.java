59de292edc9370d60a4e3519afcde6c4d13eeee4
simonebuga <simone.buga@student.tuiasi.ro>
2025-02-24 19:32:38 +0200

    cevs

---
+++ eclipse-workspace/lab2pip/src/lab2pip/Teacher.java
@@ -0,0 +1,25 @@
+package lab2pip;
+
+public class Teacher {
+	Teacher(){
+		System.out.println("Ctr. din Teacher");
+	}
+	
+	@Override
+	public String toString() {
+
+		return "from Teacher";
+	}
+	
+	
+	public String toClassroom() {
+
+		return "from Teacher";
+	}
+	
+		
+	public String toStudentPIP() {
+
+		return "from Teacher";
+	}
+}
